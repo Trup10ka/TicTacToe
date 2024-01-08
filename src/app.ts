830c9a71cdb75f64a04b30b5cfff69b3802cd722
Trup10ka <lukys3036@gmail.com>
2024-01-08 11:46:47 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import http from 'http'
+import socketIO, {Socket} from 'socket.io'
+
+const port = process.env.PORT || 8000
+const app = express()
+const server = http.createServer(app)
+const io = new socketIO.Server()
+
+const playerDuos : Array<string[]> = []
+
+io.attach(server)
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('dist/public'))
+
+app.get('/', (req, res) => {
+        res.render('login')
+    }
+)
+
+io.on('playerConnection', (message, socket) => {
+        console.log(message)
+        console.log(socket)
+    }
+)
+
+server.listen(port, () => {
+        console.log("Server is running on port: " + port)
+    }
+)
---
+++ src/public/script/index.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+const io = io('http://localhost:8000')
+
+function enterInQueue()
+{
+    io.emit();
+}
