04398ae0f653ab734e09f6deb6d318e4b80a4802
Trup10ka <lukys3036@gmail.com>
2024-01-15 11:34:59 +0100

    Implement dynamic generation of grid

---
+++ src/public/script/gamecontrols.ts
@@ -0,0 +1,23 @@
+const playgroundGrid = document.getElementById("playground-grid") as HTMLDivElement;
+
+preparePlayground()
+
+function preparePlayground() {
+    const gridSize = getSizeOfGridFromDocumentName();
+
+    for (let i = 0; i < gridSize * gridSize; i++)
+    {
+        const gameTileDiv = document.createElement("div");
+        gameTileDiv.classList.add("game-tile");
+        playgroundGrid.appendChild(gameTileDiv);
+    }
+    console.log("done preparing playground")
+}
+
+
+function getSizeOfGridFromDocumentName(): number
+{
+    let path = window.location.pathname;
+
+    return +path.split("/").pop()!.split('x')[1];
+}
