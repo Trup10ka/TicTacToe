c2136523965a83cfaef0253883ae5c2de31f4afb
Trup10ka <lukys3036@gmail.com>
2024-01-19 08:56:17 +0100

    Implement GameData class

---
+++ src/tictactoe/data/gamedata.ts
@@ -0,0 +1,12 @@
+import { GameMode } from "../gamemode/gamemode";
+
+export class GameData
+{
+   constructor(
+       public readonly gameMode: GameMode,
+       public readonly gameName?: string,
+       public readonly password?: string,
+   )
+   {
+   }
+}
