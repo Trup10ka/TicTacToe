8d0288a891cef6040d32fb611436f74108bf1f28
Trup10ka <lukys3036@gmail.com>
2024-01-21 23:51:08 +0100

    Implement game create script logic

--- src/public/script/gamecreate.ts
+++ src/public/script/gamecreate.ts
@@ -4,7 +4,54 @@ function showGameModes()
 {
     gameModeChoice.classList.toggle("show");
 }
-
-function gameCreate()
+function chooseGameMode(gameMode: string)
 {
+    const gameModeButton = document.getElementById("dropdown-content-button")! as HTMLButtonElement
+    gameModeButton.innerText = gameMode
+    showGameModes()
+}
+
+async function gameCreate() : Promise<void>
+{
+    const gameData = collectData()
+
+    const response = await fetch("/create-session", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    gameName: gameData.gameName,
+                    gameMode: gameData.gameMode,
+                    gameSize: gameData.gameSize,
+                    password: gameData.password
+                }
+            )
+        }
+    )
+    if (response.ok)
+    {
+        const gameId = response.headers.get("gameId")
+        window.location.href = `/game?id=${gameId}`
+    }
+}
+
+function collectData() : { gameName: string, password: string, gameMode: string, gameSize: number }
+{
+    const gameName = document.getElementById("game-name-input")! as HTMLInputElement
+    const password = document.getElementById("game-password-input")! as HTMLInputElement
+    const gameMode = document.getElementById("dropdown-content-button")! as HTMLButtonElement
+    const gameSize = queryGameSize(document.getElementById("game-size-form")! as HTMLFormElement)
+
+    return { gameSize: gameSize, password: password.value, gameName: gameName.value, gameMode: gameMode.innerText.toUpperCase() }
+}
+
+function queryGameSize(form: HTMLFormElement) : number
+{
+    for (const element of form.elements)
+    {
+        if (element instanceof HTMLInputElement && element.checked)
+            return parseInt((element.parentElement! as HTMLLabelElement).innerText.split("x")[0])
+    }
+    return -1
 }
