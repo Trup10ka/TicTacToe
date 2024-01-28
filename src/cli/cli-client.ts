48e96d794e90a55d2653d238c595bda7c2288c2b
Trup10ka <lukys3036@gmail.com>
2024-01-28 01:14:08 +0100

    Implement initializing command map

--- src/cli/cli-client.ts
+++ src/cli/cli-client.ts
@@ -1,13 +1,19 @@
 import { createInterface, Interface } from "readline";
-import {logger} from "../app";
-
+import { logger } from "../app";
+import { Command } from "./commands/command";
+import { HelpCommand } from "./commands/help-command";
+import { ShutdownCommand } from "./commands/shutdown-command";
 export class CLIClient
 {
+
     private reader: Interface
+
+    private commandMap: Map<string[], Command> = new Map()
 
     private constructor(reader: Interface)
     {
         this.reader = reader
+        this.initializeCommands()
     }
 
     public static initialize(): CLIClient
@@ -23,6 +29,17 @@ export class CLIClient
     {
         this.reader.on('line', (line) => {
                 console.log(`Received line`)
+            }
+        )
+    }
+
+    private initializeCommands()
+    {
+        const commands: Command[] = [
+            new HelpCommand(), new ShutdownCommand()
+        ]
+        commands.forEach(command => {
+                this.commandMap.set(command.options, command)
             }
         )
     }
