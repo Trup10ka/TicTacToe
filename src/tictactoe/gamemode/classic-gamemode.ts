fd949f153160b48294da78538d14ab4285f86d13
Trup10ka <lukys3036@gmail.com>
2024-01-30 13:22:17 +0100

    Fix - all GameMode methods public

--- src/tictactoe/gamemode/classic-gamemode.ts
+++ src/tictactoe/gamemode/classic-gamemode.ts
@@ -2,17 +2,17 @@ import { GameMode } from "./gamemode";
 
 export class ClassicGamemode implements GameMode
 {
-    checkWinCondition(board: number[][]): boolean
+    public checkWinCondition(board: number[][]): boolean
     {
         throw new Error("Method not implemented.")
     }
 
-    placeGameTile(x: number, y: number): boolean
+    public placeGameTile(x: number, y: number): boolean
     {
         throw new Error("Method not implemented.")
     }
 
-    nextPlayer(): boolean
+    public nextPlayer(): boolean
     {
         throw new Error("Method not implemented.")
     }
