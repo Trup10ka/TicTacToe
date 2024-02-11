ba2bb8ec036dad365e4d6d230c67d1ecdf4e279c
Trup10ka <lukys3036@gmail.com>
2024-02-11 18:44:53 +0100

    Implement classic-gamemode check

--- src/tictactoe/gamemode/classic-gamemode.ts
+++ src/tictactoe/gamemode/classic-gamemode.ts
@@ -1,12 +1,30 @@
 import { GameMode } from "./gamemode"
 import { PlaceTileResult } from "../session/place-tile-result"
 import { Symbol } from "../data/symbol"
+import {
+    consecutiveToWin,
+    hasConsecutiveSymbolInColumn,
+    hasConsecutiveSymbolInDiagonal, hasConsecutiveSymbolInRow
+} from "../../util/gamemode/game-mode-utill";
+
 
 export class ClassicGamemode implements GameMode
 {
     public checkWinCondition(board: number[][]): Symbol
     {
-        throw new Error("Method not implemented.")
+        const boardSize = board.length
+        const diagonalCheck = hasConsecutiveSymbolInDiagonal(board, consecutiveToWin(boardSize))
+        const verticalCheck = hasConsecutiveSymbolInColumn(board, consecutiveToWin(boardSize))
+        const horizontalCheck = hasConsecutiveSymbolInRow(board, consecutiveToWin(boardSize))
+
+        if (diagonalCheck !== Symbol.EMPTY)
+            return diagonalCheck
+        else if (verticalCheck !== Symbol.EMPTY)
+            return verticalCheck
+        else if (horizontalCheck !== Symbol.EMPTY)
+            return horizontalCheck
+        else
+            return Symbol.EMPTY
     }
 
     public canPlaceTile(symbolAtChosenCords: Symbol): PlaceTileResult
